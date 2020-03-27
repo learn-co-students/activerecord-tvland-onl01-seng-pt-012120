38760f269225cab9c059f6a6f9adb4e5e4006ac1
Roberto Livi <robertomlivi@outlook.com>
2020-03-27 20:54:47 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,21 @@
 class Actor < ActiveRecord::Base
   
+  has_many :characters
+  has_many :shows, through: :characters
+  
+  def full_name
+   "#{self.first_name} #{self.last_name}"
+  end
+  
+  def list_roles
+    list_names = []
+    self.characters.each {|c| list_names << c.name}.uniq
+    
+    list_shows = []
+    self.shows.each {|s| list_shows << s.name}.uniq
+    
+    roles = []
+    list_names.map {|name| roles << name + " - " + list_shows[0]}.flatten.uniq
+    
+  end
 end
