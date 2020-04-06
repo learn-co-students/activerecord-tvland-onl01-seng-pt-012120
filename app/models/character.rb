8e515d291a7a76f74bbc2b7fc3113786b7e25f1d
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-04-06 13:44:00 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,27 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor 
+  belongs_to :show
   
+ def say_that_thing_you_say 
+  self.name + " always says: "+ self.catchphrase
+ end
+   
+  def build_show(name:)
+    new_show = Show.create(name: name)
+    self.show = new_show
+  end
+  
+  def build_network(call_letters:)
+    new_network = Network.create(call_letters: call_letters)
+    
+  end   
 end
+
+
+#   it "can chain-build associations to which it belongs" do
+#     malcolm = Character.new(name: "Malcolm Reynolds")
+#     malcolm.build_show(name: "Firefly").build_network(:call_letters => "Fox")
+#     show = malcolm.show
+#     expect(show.name).to eq("Firefly")
+#     expect(show.network.call_letters).to eq("Fox")
+#   end
