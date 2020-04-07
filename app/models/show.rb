7e899e90b874b54d94b96fc568783af97fd0e108
mfioran1 <maurice97f@gmail.com>
2020-04-07 14:53:07 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    actors = []
+    first_name = self.actors.map {|actor| actor.first_name}
+    last_name = self.actors.map {|actor| actor.last_name}
+    combine = first_name.concat(last_name)
+    actors << combine.join(" ")
+    actors
+  end
 end
