2a1cebcd80e5b66342522a01d856fe9466a165da
felipelfukuda <felipelfukuda@gmail.com>
2020-04-14 13:21:13 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,16 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+   self.actors.collect do |act|
+    act.full_name
+   end
+  end
+
+  def built_network(args)
+    binding.pry
+    self.network = Network.find_or_create_by(args)
+  end
 end
