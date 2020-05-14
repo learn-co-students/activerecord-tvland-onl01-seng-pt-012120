06114b3cb58f9eb683ad4c481ed8d1cb6e724761
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-05-14 19:26:56 +0000

    Done.

--- db/migrate/006_add_catchphrase_to_character.rb
+++ db/migrate/006_add_catchphrase_to_character.rb
@@ -1,5 +1,5 @@
 class AddCatchphraseToCharacter < ActiveRecord::Migration[5.2]
   def change
-    add_column :catchphrase, :character, :string
+    add_column :characters, :catchphrase, :string
   end
 end
--- db/migrate/007_catchphrase.rb
+++
@@ -1,7 +0,0 @@
-class Catchphrase < ActiveRecord::Migration[5.2]
-  def change
-    create_table :catchphrase do |t|
-      t.string :catchphrase
-    end
-  end
-end
